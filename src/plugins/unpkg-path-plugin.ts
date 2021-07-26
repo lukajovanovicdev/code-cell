820e028c6253ea0853cc6937c9f04a2a45a0354c
LUKA JOVANOVIC <lukajovanovicdev@gmail.com>
2021-07-26 22:04:37 +0200

    Cache packages with localforage into IndexedDB

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,12 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
+
+//create database in IndexedDB
+const fileCache = localForage.createInstance({
+  //name of database that we wenna create
+  name: 'filecache',
+});
 
 export const unpkgPathPlugin = () => {
   return {
@@ -34,12 +41,24 @@ export const unpkgPathPlugin = () => {
           };
         }
 
+        // check if we already fetched this file and if it's in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+        // if it is return it
+        if (cachedResult) return cachedResult;
+
         const { data, request } = await axios.get(args.path);
-        return {
+
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
+
+        // store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
