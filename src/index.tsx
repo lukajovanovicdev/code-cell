cb9fb0b0a926906587f66cb6754e171a4fef913d
LUKA JOVANOVIC <lukajovanovicdev@gmail.com>
2021-07-26 21:26:15 +0200

    Implement In-Browser Bundling

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,47 @@
+import { useState, useEffect, useRef } from 'react';
 import ReactDom from 'react-dom';
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
-  return <h1>Hi!</h1>;
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({ worker: true, wasmURL: '/esbuild.wasm' });
+  };
+
+  const onClick = async () => {
+    let result;
+    ref.current &&
+      (result = await ref.current.build({
+        entryPoints: ['index.js'],
+        bundle: true,
+        write: false,
+        plugins: [unpkgPathPlugin()],
+        define: {
+          'process.env.NODE_ENV': '"production"',
+          global: 'window',
+        },
+      }));
+
+    setCode(result.outputFiles[0].text);
+  };
+
+  return (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+        <pre>{code}</pre>
+      </div>
+    </div>
+  );
 };
 
 ReactDom.render(<App />, document.querySelector('#root'));
