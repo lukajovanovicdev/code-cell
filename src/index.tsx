69f21694c1db57c77f4a87048f609b04732954b8
LUKA JOVANOVIC <lukajovanovicdev@gmail.com>
2021-09-04 21:28:45 +0200

    Implement Text Editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import ReactDom from 'react-dom';
 import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
     </div>
   );
 };
